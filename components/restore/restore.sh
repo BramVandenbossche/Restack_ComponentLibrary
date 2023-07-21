78f4619aca12f8d76cf8106610716f503ff7de53
Bram Vandenbossche <bramvandenbossche@hotmail.com>
2023-07-21 22:02:27 +0200

    typo's

--- components/restore/restore.sh
+++ components/restore/restore.sh
@@ -83,7 +83,7 @@ else
     messages+=("$(echo_message "Restoring $backup_entry" false)")
 fi
 
-restore_output=$(ssh -i "$SSH_PRIVATE_KEY" -o StrictHostKeyChecking=no "$USER"@"$PROXMOX_HOST" "$RESTORE_CMD" "$VM_CT_ID" "$BACKUP_ENTRY" "--storage" "$TARGET_STORAGE" "--force" 2>&1 )
+restore_output=$(ssh -i "$SSH_PRIVATE_KEY" -o StrictHostKeyChecking=no "$USER"@"$PROXMOX_HOST" ""$RESTORE_CMD" "$VM_CT_ID" "$BACKUP_ENTRY" "--storage" "$TARGET_STORAGE" "--force" 2>&1 ")
 exit_status=$?
 if [[ $exit_status -ne 0 ]]; then
     messages+=("$(echo_message -e  "Failed to restore container/VM. Error: $restore_output" true)")
@@ -92,7 +92,7 @@ else
     messages+=("$(echo_message -e  "Container/VM restored successfully." false)")
 fi
 
-startctvm_output=$(ssh -i "$SSH_PRIVATE_KEY" -o StrictHostKeyChecking=no "$USER"@"$PROXMOX_HOST" "$START_CMD" "$VM_CT_ID" 2>&1 )
+startctvm_output=$(ssh -i "$SSH_PRIVATE_KEY" -o StrictHostKeyChecking=no "$USER"@"$PROXMOX_HOST" ""$START_CMD" "$VM_CT_ID" 2>&1 ")
 exit_status=$?
 if [[ $exit_status -ne 0 ]]; then
     messages+=("$(echo_message "Failed to started container/VM. Error: $startctvm_output" true)")

d95f50a479d67e1b2b58e6ccae2613190272cfb0
Bram <bramvandenbossche@hotmail.com>
2023-08-01 20:32:45 +0200

    one-liner test

--- components/update-npm/update.sh
+++ components/update-npm/update.sh
@@ -79,10 +79,7 @@ update() {
   execute_command_on_container "sudo sed -i 's+0.0.0+${RELEASE}+g' /root/nginx-proxy-manager-${RELEASE}/backend/package.json"
   execute_command_on_container "sudo sed -i 's+0.0.0+${RELEASE}+g' /root/nginx-proxy-manager-${RELEASE}/frontend/package.json"
   execute_command_on_container "sudo sed -i 's+^daemon+#daemon+g' /root/nginx-proxy-manager-${RELEASE}/docker/rootfs/etc/nginx/nginx.conf"
-  NGINX_CONFS=$(execute_command_on_container "find '/root/nginx-proxy-manager-${RELEASE}' -type f -name '*.conf' 2>&1")
-  for NGINX_CONF in $NGINX_CONFS; do
-    execute_command_on_container "sudo sed -i 's+include conf.d+include /etc/nginx/conf.d+g' \"$NGINX_CONF\""
-  done
+  execute_command_on_container "find '/root/nginx-proxy-manager-${RELEASE}' -type f -name '*.conf' -exec sudo sed -i 's+include conf.d+include /etc/nginx/conf.d+g' {} +"
   execute_command_on_container "sudo mkdir -p /var/www/html /etc/nginx/logs"
   execute_command_on_container "sudo cp -r /root/nginx-proxy-manager-${RELEASE}/docker/rootfs/var/www/html/* /var/www/html/"
   execute_command_on_container "sudo cp -r /root/nginx-proxy-manager-${RELEASE}/docker/rootfs/etc/nginx/* /etc/nginx/"
